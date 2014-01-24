415ce1cbb07a5ec900078051e525b0c863e05c13
Steven Nunez <snunez@cyrusinnovation.com>
2014-01-23 19:42:31 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OmgFriends::Application.config.secret_key_base = '8b1aab1203750d8dc599176907a66cdd7d560b786b112d83a4cb48a5f328f1e4b883cb3db82f43f879101ef146dd30dac24ce8a56d707e33c9b7b5c214a4a40b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OmgFriends::Application.config.session_store :cookie_store, key: '_omg_friends_session'
